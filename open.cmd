716cc4b2280aa9b772e756c0c2d01c6a5a22998b
dvshin <dvshin@outlook.com>
2021-04-29 20:33:29 +1000

    data sync test version

--- open.cmd
+++ open.cmd
@@ -1,1 +1,3 @@
-netsh advfirewall firewall add rule name="poble" dir=in action=allow program="Full path of .exe" enable=yes
+netsh advfirewall firewall add rule name="poble" dir=in action=allow program="C:\dv\Poble\Poble\bin\Release\net5.0-windows\publish\Poble.exe" enable=yes
+
+ netsh advfirewall firewall add rule name ="posHub" dir =in action = allow protocol = TCP localport = *
