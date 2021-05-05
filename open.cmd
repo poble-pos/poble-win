35d3e4bf32cc287a01b3b3b5c0942ea6a72770ff
dvshin <dvshin@outlook.com>
2021-05-05 10:21:31 +1000

    printer queue send hub

--- open.cmd
+++
@@ -1,3 +0,0 @@
-netsh advfirewall firewall add rule name="poble" dir=in action=allow program="C:\dv\Poble\Poble\bin\Release\net5.0-windows\publish\Poble.exe" enable=yes
-
- netsh advfirewall firewall add rule name ="posHub" dir =in action = allow protocol = TCP localport = *
